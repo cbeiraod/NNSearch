9be353838627f6252642ac83eb134ccf33d24819
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-12-07 14:14:04 +0000

    Added a check if the cfg file exists

--- runNN.py
+++ runNN.py
@@ -2,6 +2,7 @@ import commonFunctions as funk
 
 if __name__ == "__main__":
   import argparse
+  import os.path
 
   parser = argparse.ArgumentParser(description='Process the command line options')
   parser.add_argument('-d', '--dryRun', action='store_true', help='Do a dry run (i.e. do not actually run the potentially dangerous commands but print them to the screen)')
@@ -13,6 +14,11 @@ if __name__ == "__main__":
 
   if not args.dryRun:
     print "You did not enable dry run. You are on your own!"
+
+  if not os.path.isfile(args.configFile):
+    import errno
+    #raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), args.configFile)
+    raise OSError(errno.ENOENT, os.strerror(errno.ENOENT), args.configFile)
 
   import json
   configJson = json.load(open(args.configFile, "rb"))
--- validateCfg.py
+++ validateCfg.py
@@ -2,12 +2,18 @@ import commonFunctions as funk
 
 if __name__ == "__main__":
   import argparse
+  import os.path
 
   parser = argparse.ArgumentParser(description='Process the command line options')
   parser.add_argument('-c', '--configFile', required=True, help='Configuration file describing the neural network topology and options as well as the samples to process')
   parser.add_argument('-v', '--verbose', action='store_true', help='Whether to print verbose output')
 
   args = parser.parse_args()
+
+  if not os.path.isfile(args.configFile):
+    import errno
+    #raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), args.configFile)
+    raise OSError(errno.ENOENT, os.strerror(errno.ENOENT), args.configFile)
 
   import json
   configJson = json.load(open(args.configFile, "rb"))
