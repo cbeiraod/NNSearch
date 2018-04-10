6729309eee2d0d88aa7fc1d19d2dca40d22281f1
diogodebastos <diogodebastos18@gmail.com>
2018-04-10 17:50:53 +0100

    script to test @batch

--- prefold.sh
+++ prefold.sh
@@ -6,5 +6,5 @@
 export NTUPLE_DIR=/lstore/cms/cbeiraod/Stop4Body/nTuples_v2017-10-19
 export FOLDED_DIR=/lstore/cms/dbastos/Stop4Body/nTuples_v2017-10-19_folded
 
-python prefold.py --type "n-fold" --folds 3 --splitting "random" --inputDirectory ${NTUPLE_DIR} --outputDirectory ${FOLDED_DIR}
-python prefold.py --type "n-fold" --folds 6 --splitting "random" --inputDirectory ${NTUPLE_DIR} --outputDirectory ${FOLDED_DIR}
+python prefold.py --type "n-fold" --folds 3 --splitting "random" --inputDirectory ${NTUPLE_DIR} --outputDirectory ${FOLDED_DIR} --noRecurse
+python prefold.py --type "n-fold" --folds 6 --splitting "random" --inputDirectory ${NTUPLE_DIR} --outputDirectory ${FOLDED_DIR} --noRecurse
